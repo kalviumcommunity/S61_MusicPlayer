4423f96b031b8c34e46f4ab7cf2064a1889b1820
gunapriya07 <147796209+gunapriya07@users.noreply.github.com>
2024-03-07 13:32:29 +0530

    update on server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 4000;
 
 // Route for the root endpoint
 app.get('/', (req, res) => {
-  res.send(' ASAP Project - Endpoint!');
+  res.send('ASAP Project - Endpoint!');
 });
 
 // Route for the /ping endpoint
@@ -12,7 +12,16 @@ app.get('/ping', (req, res) => {
   res.send('Pong!');
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
 // Start the server
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// Export the server for testing
+module.exports = server;
