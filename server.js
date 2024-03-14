a5666d46908e45751d4a9a397375ed9f39d0cf18
gunapriya07 <147796209+gunapriya07@users.noreply.github.com>
2024-03-14 14:37:42 +0530

    coonect to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose =require("mongoose")
+const connectDB=require("./config/db")
+connectDB()
 const app = express();
-const PORT = 4000;
+const PORT = 4001;
 
 // Route for the root endpoint
 app.get('/', (req, res) => {
@@ -17,6 +20,12 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
+
+
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+})
 
 // Start the server
 const server = app.listen(PORT, () => {
