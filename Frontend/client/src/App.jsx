29bf6ed280e082a20cef9d90b83094b11fd7d376
gunapriya07 <147796209+gunapriya07@users.noreply.github.com>
2024-03-22 15:09:34 +0530

    added the data

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 // import LandPage from './Component/LandPage'
 // import Dummy from "./Component/Dummy"
-import Singer from "./Component/Singer"
+// import Singer from "./Component/Singer"
+import UserInput from './Component/UserInput';
 
 function App() {
   return (
     <>
       {/* <LandPage /> */}
       {/* <Dummy/> */}
-     <Singer/>
+     {/* <Singer/> */}
+     <UserInput/>
     </>
   )
 }
